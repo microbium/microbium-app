6d69d219d38775d1ebe3efd75bd16f763a341f12
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-10-24 16:48:53 -0700

    Update build to require clean working tree

---
+++ .electron-vue/build.sh
@@ -0,0 +1,12 @@
+run_build () {
+  node ./.electron-vue/build-bundles.js
+  electron-builder
+  terminal-notifier -title 'Bacterium' -message 'Build Complete'
+}
+
+if [[ -n $(git status --porcelain) ]]
+then
+  echo "$(tput setaf 1)Working tree is dirty, please resolve changes before building.$(tput sgr0)"
+else
+  run_build
+fi
