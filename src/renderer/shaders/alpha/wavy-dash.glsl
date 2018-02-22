c0ba66a74f5fe5519f3ef2850665de218a95dbcc
Jay Weeks <jay.patrick.weeks@gmail.com>
2018-02-22 10:27:38 -0800

    Update wavy dash function

--- src/renderer/shaders/alpha/wavy-dash.glsl
+++ src/renderer/shaders/alpha/wavy-dash.glsl
@@ -3,13 +3,15 @@ float wavyDash (
   float repeat, float offset,
   float shapeStart, float shapeEnd
 ) {
+  float PI_2 = PI / 2.0;
   vec2 coord = vec2(
-    udo.x - offset * 0.025,
+    (udo.x + 1.0) * 0.5,
     udo.y);
 
   float lineWidth = max(0.0, sin(coord.y * 0.2) * 0.4 + 0.5);
-  float offsetX = sin(coord.y * 0.2) * 0.2;
-  float lineStep = sin(((coord.x + offsetX) * PI * 2.0) * repeat) *
+  float offsetX = sin(coord.y * 0.2) *
+    (sin(offset * 0.025) * 0.1 + 0.2);
+  float lineStep = sin(((coord.x + offsetX) * PI + PI_2 * 1.2) * repeat) *
     (shapeEnd - shapeStart) + shapeStart;
   float line = pow(smoothstep(1.0 - lineWidth, 1.0, lineStep), 0.5);
 
