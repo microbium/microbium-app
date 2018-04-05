29c44c43d5c01093516b8940017e218d4be04eaf
Jay Weeks <jay.patrick.weeks@gmail.com>
2018-04-05 16:50:27 -0700

    Update scene serialization

--- src/renderer/components/editor/compositor/io.js
+++ src/renderer/components/editor/compositor/io.js
@@ -3,28 +3,84 @@ import { map, flatten2, expand2 } from '@src/utils/array'
 import { roundToPlaces } from '@src/utils/number'
 import { createControlsState } from '@src/store/modules/Palette'
 
-const ABBRV_KEY_MAP = {
-  'sw': 'strokeWidth',
-  'sc': 'strokeColor',
-  'sa': 'strokeAlpha',
-  'sm': 'strokeWidthMod',
-  'mt': 'inputModTypeIndex',
-  'ct': 'constraintIndex',
-  'si': 'styleIndex',
+const ABBRV_KEYS = [
+  'activeMode',
+  'activePalettes',
+  'alphaFactor',
+  'alphaFuncIndex',
+  'alphaTextureIndex',
+  'banding',
+  'bandStep',
+  'bloom',
+  'blurPasses',
+  'blurStep',
+  'bufferScale',
+  'clear',
+  'colorHex',
+  'colorShift',
+  'connectedIndices',
+  'constraintGroups',
+  'constraintIndex',
+  'controls',
+  'curve',
+  'dashFunction',
+  'edges',
+  'engineCadence',
+  'engineFlex',
+  'forces',
+  'geometry',
+  'index',
+  'indices',
+  'inputModTypeIndex',
+  'intensity',
+  'intensityFactor',
+  'intensityTypeIndex',
+  'isClosed',
+  'isComplete',
+  'lineLengths',
+  'lineTool',
+  'linkSizeAvg',
+  'modifiers',
+  'name',
+  'noise',
+  'offset',
+  'polarAngle',
+  'polarIterations',
+  'polarOffset',
+  'positionTypeIndex',
+  'postEffects',
+  'radius',
+  'scale',
+  'segMaxLength',
+  'segments',
+  'segMinLength',
+  'slipTolerance',
+  'strokeAlpha',
+  'strokeColor',
+  'strokeWidth',
+  'strokeWidthMod',
+  'strokeWidthMod',
+  'strokeWidthModulations',
+  'styleIndex',
+  'styles',
+  'subDivisions',
+  'textureIndex',
+  'thickness',
+  'thickness',
+  'tintAlpha',
+  'tintHex',
+  'typeIndex',
+  'useScreenTintFunc',
+  'vertices',
+  'viewport'
+]
 
-  'ii': 'indices',
-  'ci': 'connectedIndices',
-  'la': 'linkSizeAvg',
-  'll': 'lineLengths',
-  'wm': 'strokeWidthModulations',
-  'cl': 'isClosed',
-  'co': 'isComplete',
+const ABBRV_KEY_MAP = ABBRV_KEYS
+  .reduce((map, key, index) => {
+    map[index + ''] = key
+    return map
+  }, {})
 
-  'ge': 'geometry',
-  'cn': 'controls',
-  'sg': 'segments',
-  'vt': 'vertices'
-}
 const KEY_ABBRV_MAP = Object.keys(ABBRV_KEY_MAP)
   .reduce((map, key) => {
     map[ABBRV_KEY_MAP[key]] = key
@@ -39,6 +95,7 @@ export function createIOController (tasks, state) {
   const io = {
     serializeScene () {
       const mapKeys = io.mapKeys.bind(null, KEY_ABBRV_MAP)
+      const mapKeysDeep = io.mapKeysDeep.bind(null, KEY_ABBRV_MAP)
       const { geometry, controls, viewport } = state
       const { segments, vertices } = geometry
 
@@ -54,22 +111,26 @@ export function createIOController (tasks, state) {
       const verticesOut = io.serializeArray(flatten2(vertices), 4)
 
       return mapKeys({
-        viewport: {
+        viewport: mapKeys({
           offset: io.serializeArray(viewport.offset, 4),
           scale: roundToPlaces(viewport.scale, 4)
-        },
+        }),
         geometry: mapKeys({
           segments: segmentsOut,
           vertices: verticesOut
         }),
-        controls: Object.assign({}, controls)
+        controls: mapKeysDeep(controls)
       })
     },
 
     deserializeScene (json) {
       const unmapKeys = io.mapKeys.bind(null, ABBRV_KEY_MAP)
-      const { geometry, controls, viewport } = unmapKeys(json)
-      const { segments, vertices } = unmapKeys(geometry)
+      const unmapKeysDeep = io.mapKeysDeep.bind(null, ABBRV_KEY_MAP)
+
+      const data = unmapKeys(json)
+      const { segments, vertices } = unmapKeys(data.geometry)
+      const viewport = unmapKeys(data.viewport)
+      const controls = unmapKeysDeep(data.controls)
       const defaultControls = createControlsState()
 
       const segmentsOut = segments
@@ -101,8 +162,31 @@ export function createIOController (tasks, state) {
     },
 
     mapKeys (map, src) {
+      if (typeof src !== 'object') return src
+
       return Object.keys(src).reduce((out, key) => {
         out[map[key] || key] = src[key]
+        return out
+      }, {})
+    },
+
+    mapKeysDeep (map, src) {
+      if (typeof src !== 'object') return src
+
+      return Object.keys(src).reduce((out, key) => {
+        const mappedKey = map[key] || key
+        const val = src[key]
+
+        if (typeof val === 'object') {
+          if (Array.isArray(val)) {
+            out[mappedKey] = val.map((innerVal) => io.mapKeysDeep(map, innerVal))
+          } else {
+            out[mappedKey] = io.mapKeysDeep(map, val)
+          }
+        } else {
+          out[mappedKey] = val
+        }
+
         return out
       }, {})
     },
