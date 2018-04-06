8f09a9a4f806fdb1b373611c6f8f03463edb7ff0
Jay Weeks <jay.patrick.weeks@gmail.com>
2018-04-05 17:23:29 -0700

    Make exported html background always gray for now

--- src/main/exporters/html/index.js
+++ src/main/exporters/html/index.js
@@ -26,14 +26,13 @@ function toTitleCase (str) {
     (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())
 }
 
-// TODO: Improve scene data accessors (cn -> controls)
 export function exportSceneHTML (destPath, sceneData) {
   return getTemplate('./template.ejs')
     .then((template) => {
       const subTitle = toTitleCase(
         basename(destPath, '.html').replace(/[-_]+/g, ' '))
       const rawSceneData = JSON.stringify(sceneData)
-      const backgroundColor = sceneData.cn.postEffects.clear.colorHex
+      const backgroundColor = '#222222'
 
       return template({
         API_VERSION,
