10c0ac6f233d5cecda2b3984f916250cdecaf612
Jay Weeks <jay.patrick.weeks@gmail.com>
2018-10-15 21:33:23 -0700

    Fix stats animation overflow

--- src/renderer/components/editor/Compositor.vue
+++ src/renderer/components/editor/Compositor.vue
@@ -42,6 +42,13 @@
 
 <style lang="scss">
 .editor-compositor {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
   &__scene {
     position: absolute;
     top: 0;
