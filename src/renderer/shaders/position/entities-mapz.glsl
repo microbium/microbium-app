7c3cc226605828fb4d28c0359ff02bd5f8c698bb
Jay Weeks <jay.patrick.weeks@gmail.com>
2019-05-24 09:23:15 -0700

    Update z offset mapping

--- src/renderer/shaders/position/entities-mapz.glsl
+++ src/renderer/shaders/position/entities-mapz.glsl
@@ -1,7 +1,9 @@
 float mapZ (vec2 pos, float id) {
-  float offset = max(0.0, 1.0 - smoothstep(0.0, 100.0, length(pos)));
-  float offset2 = offset * offset;
-  return id * 2.0 + offset2 * 1000.0;
+  float offset = length(pos);
+  float t = smoothstep(0.0, 1.0, offset / 1400.0);
+  float t0 = t * t;
+  float offset3 = sin(t * 4.0) * 300.0;
+  return -(offset * t0 * 0.4) + (offset3) + -(id * 8.0);
 }
 
 #pragma glslify: export(mapZ)
